fd6bb2eb5bedcc7d4db5bca3be3605d6e123f0ce
Kim Young Jae <corea_kyj@naver.com>
2018-06-14 17:37:50 +0900

    메소드명 수정 및 private 일부 추가 ( 테스트 X )

--- virustotal_file.py
+++ virustotal_file.py
@@ -1,4 +1,4 @@
-import requests, json, os, time
+import requests, json, os, time, sys
 
 LIST_OF_HTTP_STATUS_CODES = {
     100 : "Continue", 101 : "Switching Protocols", 102 : "Processing", 103 : "Early Hints",
@@ -20,22 +20,21 @@ LIST_OF_HTTP_STATUS_CODES = {
 INTERVAL_TIME = 0.05
 
 class VirusTotalFile :
-    def __init__(self, api_key) :
-        self.__api_key = api_key
+    def __init__(self, apikey) :
+        self.__apikey = apikey
 
     # Public, Private
-    def sending_and_scanning_file(self, src_path) :
+    def scan(self, file_path) :
         ret = False
-        if os.path.getsize(src_path) >= 32000000:
+        if os.path.getsize(file_path) >= 32000000:
             print('File size limit is 32MB')
             return ret
         try:
-            url = 'https://www.virustotal.com/vtapi/v2/file/scan'
-            params = {'apikey': self.__api_key}
-            src_path = src_path.replace(os.sep, '/')
-            with open(src_path, 'rb') as f:
-                files = {'file': (src_path, f)}
-                response = requests.post(url, files=files, params=params)
+            params = {'apikey': self.__apikey}
+            file_path = file_path.replace(os.sep, '/')
+            with open(file_path, 'rb') as f:
+                files = {'file': (file_path, f)}
+                response = requests.post('https://www.virustotal.com/vtapi/v2/file/scan', files=files, params=params)
                 time.sleep(INTERVAL_TIME)
             if response.status_code == 200:
                 response_json = response.json()
@@ -59,10 +58,60 @@ class VirusTotalFile :
             time.sleep(INTERVAL_TIME)
             return ret
 
-    # Public, Private
-    def rescanning_already_submitted_files(self, hash_str):
+    # Private ( Not Tested )
+    def scan_upload_url(self, file_path):
         ret = False
-        params = {'apikey': self.key, 'resource': hash_str}
+        if os.path.getsize(file_path) >= 32000000:
+            print('File size limit is 32MB')
+            return ret
+        try:
+            params = {'apikey': self.__apikey}
+            response = requests.get('https://www.virustotal.com/vtapi/v2/file/scan/upload_url', params=params)
+            time.sleep(INTERVAL_TIME)
+            if response.status_code == 200:
+                json_response = response.json()
+                upload_url = json_response['upload_url']
+                file_path = file_path.replace(os.sep, '/')
+                with open(file_path, 'rb') as f:
+                    files = {'file': (file_path.decode('utf-8'), open(file_path, 'rb'))}
+                    response = requests.post(upload_url, files=files)
+                    time.sleep(INTERVAL_TIME)
+                    if response.status_code == 200:
+                        pass
+                    elif response.status_code == 204:
+                        print('Request rate limit exceeded. You are making more requests than allowed.')
+                    elif response.status_code == 400:
+                        print(
+                            'Bad request. Your request was somehow incorrect. This can be caused by missing arguments or arguments with wrong values.')
+                    elif response.status_code == 403:
+                        print(
+                            'Forbidden. You don\'t have enough privileges to make the request. You may be doing a request without providing an API key or you may be making a request to a Private API without having the appropriate privileges.')
+                    else:
+                        print("HTTP Request Error{} : {}".format(response.status_code,
+                                                                 LIST_OF_HTTP_STATUS_CODES.get(response.status_code,
+                                                                                               'Unknown')))
+            elif response.status_code == 204:
+                print('Request rate limit exceeded. You are making more requests than allowed.')
+            elif response.status_code == 400:
+                print(
+                    'Bad request. Your request was somehow incorrect. This can be caused by missing arguments or arguments with wrong values.')
+            elif response.status_code == 403:
+                print(
+                    'Forbidden. You don\'t have enough privileges to make the request. You may be doing a request without providing an API key or you may be making a request to a Private API without having the appropriate privileges.')
+            else:
+                print("HTTP Request Error{} : {}".format(response.status_code,
+                                                         LIST_OF_HTTP_STATUS_CODES.get(response.status_code,
+                                                                                       'Unknown')))
+            pass
+        except Exception as e:
+            print(e)
+        finally:
+            return ret
+
+    # Public, Private
+    def rescan(self, resource):
+        ret = False
+        params = {'apikey': self.__apikey, 'resource': resource}
         try:
             response = requests.post('https://www.virustotal.com/vtapi/v2/file/rescan', params=params)
             time.sleep(INTERVAL_TIME)
@@ -90,11 +139,40 @@ class VirusTotalFile :
         finally:
             return ret
 
-    def retrieving_file_scan_report(self, hash_str, dst_path):
+    # Private ( Not Tested )
+    def rescan_delete(self, resource):
+        ret = False
+        params = {'apikey': self.__apikey, 'resource': resource}
+        try :
+            response = requests.post('https://www.virustotal.com/vtapi/v2/file/rescan/delete', params=params)
+            time.sleep(INTERVAL_TIME)
+            if response.status_code == 200:
+                response_json = response.json()
+                response_code = response_json['response_code']
+                if response_code == 1:
+                    print('The scheduled scan deletion succeed.')
+                    ret = True
+                elif response_code == -1 :
+                    print('The scheduled scan deletion failed for whatever reason.')
+            elif response.status_code == 204:
+                print('Request rate limit exceeded. You are making more requests than allowed.')
+            elif response.status_code == 400:
+                print('Bad request. Your request was somehow incorrect. This can be caused by missing arguments or arguments with wrong values.')
+            elif response.status_code == 403:
+                print('Forbidden. You don\'t have enough privileges to make the request. You may be doing a request without providing an API key or you may be making a request to a Private API without having the appropriate privileges.')
+            else:
+                print("HTTP Request Error{} : {}".format(response.status_code,LIST_OF_HTTP_STATUS_CODES.get(response.status_code,'Unknown')))
+        except Exception as e:
+            print(e)
+        finally:
+            return ret
+
+    # Public, Private
+    def report(self, resource, dst_path):
         ret = False
         try:
             url = 'https://www.virustotal.com/vtapi/v2/file/report'
-            params = {'apikey': self.key, 'resource': hash_str}
+            params = {'apikey': self.__apikey, 'resource': resource}
             response = requests.get(url, params=params)
             time.sleep(INTERVAL_TIME)
             if response.status_code == 200:
@@ -102,8 +180,8 @@ class VirusTotalFile :
                 response_code = response_json['response_code']
                 if response_code == 1:
                     print(response_json['verbose_msg'])
-                    with open(os.path.join(dst_path, hash_str + '.json'), 'w', encoding='utf8') as f:
-                        json.dump(response.json(), f, ensure_ascii=False, indent=4)
+                    with open(os.path.join(dst_path, resource + '.json'), 'w', encoding='utf8') as f:
+                        json.dump(response_json, f, ensure_ascii=False, indent=4)
                     ret = True
                 else :
                     print('The event of some unexpected error')
@@ -120,3 +198,82 @@ class VirusTotalFile :
             print(e)
         finally:
             return ret
+
+    # Private
+    def behaviour(self, hash, dst_path) :
+        ret = False
+        params = {'apikey': self.__apikey, 'hash': hash}
+        headers = {
+            "Accept-Encoding": "gzip, deflate",
+            "User-Agent": "gzip,  My Python requests library example client or username"
+        }
+        try :
+            response = requests.get('https://www.virustotal.com/vtapi/v2/file/behaviour', params=params, headers=headers)
+            if response.status_code == 200 :
+                response_json = response.json()
+                response_code = response_json['response_code']
+                if response_code == 1 :
+                    print(response_json['verbose_msg'])
+                    with open(os.path.join(dst_path, hash + '.json'), 'w', encoding='utf8') as f:
+                        json.dump(response_json, f, ensure_ascii=False, indent=4)
+                        ret = True
+                elif response_code == 0 :
+                    print(response_json['verbose_msg'])
+
+            elif response.status_code == 204:
+                print('Request rate limit exceeded. You are making more requests than allowed.')
+            elif response.status_code == 400:
+                print('Bad request. Your request was somehow incorrect. This can be caused by missing arguments or arguments with wrong values.')
+            elif response.status_code == 403:
+                print('Forbidden. You don\'t have enough privileges to make the request. You may be doing a request without providing an API key or you may be making a request to a Private API without having the appropriate privileges.')
+            else:
+                print("HTTP Request Error{} : {}".format(response.status_code,LIST_OF_HTTP_STATUS_CODES.get(response.status_code,'Unknown')))
+        except Exception as e :
+            print(e)
+        finally:
+            return ret
+
+    # Private
+    def network_traffic(self, hash):
+        pass
+
+    # Private
+    def search(self, query):
+        pass
+
+    # Private
+    def clusters(self, date):
+        pass
+
+    # Special
+    def feed(self, package):
+        pass
+
+    # Private
+    def download(self, hash, dst_path) :
+        ret = False
+        params = {'apikey': self.__apikey, 'hash': hash}
+        try :
+            response = requests.get('https://www.virustotal.com/vtapi/v2/file/download', params=params)
+            time.sleep(INTERVAL_TIME)
+            if response.status_code == 200 :
+                downloaded_file = response.content
+                with open(os.path.join(dst_path, hash + '.vir'), 'wb') as f :
+                    f.write(downloaded_file.encode('utf-8'))
+                    ret = True
+            elif response.status_code == 204:
+                print('Request rate limit exceeded. You are making more requests than allowed.')
+            elif response.status_code == 400:
+                print('Bad request. Your request was somehow incorrect. This can be caused by missing arguments or arguments with wrong values.')
+            elif response.status_code == 403:
+                print('Forbidden. You don\'t have enough privileges to make the request. You may be doing a request without providing an API key or you may be making a request to a Private API without having the appropriate privileges.')
+            else:
+                print("HTTP Request Error{} : {}".format(response.status_code,LIST_OF_HTTP_STATUS_CODES.get(response.status_code,'Unknown')))
+        except Exception as e :
+            print(e)
+        finally :
+            return ret
+
+    # antivirus vendors
+    def false_positives(self, limit):
+        pass
